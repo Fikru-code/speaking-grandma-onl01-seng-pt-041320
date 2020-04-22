a56081e007411219842e77d28dbecafbfad8fbe4
Fikru Ashagre <fikruashagre@gmail.com>
2020-04-22 19:40:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,12 @@
 def speak_to_grandma (phrase)
-  if Phrase ="I LOVE YOU GRANDMA!"
+  
+  if the Phrase ="I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!
-     elsif phrase!=phrase.upcase
+     elsif the phrase!=phrase.upcase
    HUH?! SPEAK UP, SONNY!
 else
      NO, NOT SINCE 1938!
-    
-  end 
-  def speak_to _grandma ("I LOVE YOU GRANDMA!")
-end
-
-  
-  
+    end
 end
 
   
